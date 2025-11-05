23146c35c265100e770a3bccb99e5831a38c6203
Ridhma21 <ridhimachebolu21@gmail.com>
2025-11-06 00:08:20 +0530

    Create Q10.c

---
+++ Q10.c
@@ -0,0 +1,16 @@
+// Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+#include <stdio.h>
+
+int main() {
+    int sec, h, m, s;
+    printf("Input seconds: ");
+    scanf("%d", &sec);
+
+    h = sec / 3600;
+    m = (sec % 3600) / 60;
+    s = sec % 60;
+
+    printf("Time is %d:%d:%d\n", h, m, s);
+    return 0;
+}
