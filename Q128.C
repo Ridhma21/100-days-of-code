5e9c9b2aba879e1531c25aab20d8bb27fc7e298b
Ridhma21 <ridhimachebolu21@gmail.com>
2025-11-26 19:37:25 +0530

    Create Q128.C

---
+++ Q128.C
@@ -0,0 +1,27 @@
+//Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp = fopen("input.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    char ch;
+    int vowels = 0, consonants = 0;
+    while ((ch = fgetc(fp)) != EOF) {
+        if (isalpha(ch)) {
+            ch = tolower(ch);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    printf("Vowels: %d\nConsonants: %d\n", vowels, consonants);
+    fclose(fp);
+    return 0;
+}
